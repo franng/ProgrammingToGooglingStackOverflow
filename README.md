b4d921a05e13cb53b5e21b6b2043538f35e1b9d0
Francis Nguyen <franng@vsp.com>
2015-07-24 13:47:58 -0700

    First commit.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ProgrammingToGooglingStackOverflow
+
+## Summary
+
+This is the source of the Chrome extension for replacing "Programming" with "Googling StackOverflow" (as well as variants.)
+
+## Credits
+
+* Put together by Francis N
+* Motivated by Ryan J
+* Inspired by The Allium: http://www.theallium.com/engineering/computer-programming-to-be-officially-renamed-googling-stackoverflow/
+* Shout-out to Cloud to Butt: https://github.com/panicsteve/cloud-to-butt
---
+++ content_script.js
@@ -0,0 +1,48 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from Cloud to Butt
+	// which stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bProgramming\b/g, "Googling StackOverflow");
+	v = v.replace(/\bprogramming\b/g, "googling StackOverflow");
+	v = v.replace(/\bProgrammer\b/g, "Googler of StackOverflow");
+	v = v.replace(/\bprogrammer\b/g, "googler of StackOverflow");
+	v = v.replace(/\bProgram\b/g, "Google StackOverflow");
+	v = v.replace(/\bprogram\b/g, "google StackOverflow");
+	v = v.replace(/\bPrograms\b/g, "Googles StackOverflow");
+	v = v.replace(/\bprograms\b/g, "googles StackOverflow");
+	v = v.replace(/\bProgrammed\b/g, "Googled StackOverflow");
+	v = v.replace(/\bprogrammed\b/g, "googled StackOverflow");
+	
+	textNode.nodeValue = v;
+}
+
